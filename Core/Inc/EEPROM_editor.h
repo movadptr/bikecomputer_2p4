c68bedc6894c193b22dca7dbf2079021a28a644b
movadptr <148840314+movadptr@users.noreply.github.com>
2025-04-10 13:26:03 +0200

    Create EEPROM_editor.h

---
+++ Core/Inc/EEPROM_editor.h
@@ -0,0 +1,19 @@
+/*
+ * EEPROM_editor.h
+ *
+ * Created on: Apr 10, 2025
+ * Author: POS1BP
+ */
+
+#ifndef INC_EEPROM_EDITOR_H_
+#define INC_EEPROM_EDITOR_H_
+
+#define DISPLAYED_MEM_SECTION_SIZE 20U
+
+void EEPROM_editor(void);
+void printEditorContent(uint8_t* buff, uint8_t startindx);
+void printByte(uint8_t* buff, uint8_t startindx, uint8_t indx);
+void printCursor(uint8_t startindx, uint8_t indx);
+void modifyValue(uint8_t* buff, uint8_t indx, uint8_t startindx);
+
+#endif /* INC_EEPROM_EDITOR_H_ */
