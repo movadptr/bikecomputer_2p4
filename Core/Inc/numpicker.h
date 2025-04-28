2bb88425689e6893635c774e867f9bd509995f76
movadptr <148840314+movadptr@users.noreply.github.com>
2025-04-28 09:35:44 +0200

    Update numpicker.h
    add print in place variant and hex number handling

--- Core/Inc/numpicker.h
+++ Core/Inc/numpicker.h
@@ -2,7 +2,7 @@
  * numpicker.h
  *
  *  Created on: Apr 11, 2025
- *  Author: POS1BP
+ *  Author: Póti Szabolcs
  */
 
 #ifndef INC_NUMPICKER_H_
@@ -10,8 +10,11 @@
 
 #include "main.h"
 
+uint8_t assembleHexNum(uint8_t* numtf);
+void disassembleHexNum(uint8_t* numtf, uint8_t val);
 uint32_t assembleU32Num(uint8_t* numtf);
 void disassembleU32Num(uint8_t* numtf, uint32_t val);
 uint32_t numPickerUInt32_V(uint32_t Llimit, uint32_t Hlimit, uint32_t startval, volatile uint8_t* buttons);
+uint32_t numPickerUInt32_printInPlace_V(uint32_t Llimit, uint32_t Hlimit, uint32_t startval, volatile uint8_t* buttons, uint8_t xpos, uint8_t ypos);
 
 #endif /* INC_NUMPICKER_H_ */
