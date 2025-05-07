c85fbdcf72ac2a64273a55e6aa60bab243d281e5
movadptr <148840314+movadptr@users.noreply.github.com>
2025-05-07 12:46:33 +0200

    add numpicker hex fn

--- Core/Inc/numpicker.h
+++ Core/Inc/numpicker.h
@@ -16,5 +16,6 @@ uint32_t assembleU32Num(uint8_t* numtf);
 void disassembleU32Num(uint8_t* numtf, uint32_t val);
 uint32_t numPickerUInt32_V(uint32_t Llimit, uint32_t Hlimit, uint32_t startval, volatile uint8_t* buttons);
 uint32_t numPickerUInt32_printInPlace_V(uint32_t Llimit, uint32_t Hlimit, uint32_t startval, volatile uint8_t* buttons, uint8_t xpos, uint8_t ypos);
+uint8_t numPickerHex_printInPlace_V(uint8_t Llimit, uint8_t Hlimit, uint8_t startval, volatile uint8_t* buttons, uint8_t xpos, uint8_t ypos);
 
 #endif /* INC_NUMPICKER_H_ */
